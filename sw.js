b02cfb0ce1b9804e1386c26b1625c575824a3660
1goguscz2003-wq <1goguscz2003@gmail.com>
2026-08-31 21:57:50 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='bop-lift-v2';const ASSETS=['./','./index.html','./call.html','./operator.html','./admin.html','./styles.css','./config.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
